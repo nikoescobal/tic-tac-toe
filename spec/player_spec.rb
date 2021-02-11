40ddb6cb53ea184ee09bb5853bcf843382aaee26
Niko Escobal <niko.escobal@gmail.com>
2021-02-11 23:37:55 +0800

    Fixed rubocop errors.

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,15 +2,15 @@ require_relative '../lib/player'
 require_relative '../lib/board'
 
 describe Player do
-  describe "initialize" do
-    it "assigns the players with a name and shape" do
-      player = Player.new("Jake", "x")
-      expect(player.name).to eq("Jake")
-      end
+  describe 'initialize' do
+    it 'assigns the players with a name and shape' do
+      player = Player.new('Jake', 'x')
+      expect(player.name).to eq('Jake')
+    end
 
-    it "assigns the players with a name and shape" do
-      player = Player.new("Chad", "o")
-      expect(player.shape).to eq("o")
-      end
+    it 'assigns the players with a name and shape' do
+      player = Player.new('Chad', 'o')
+      expect(player.shape).to eq('o')
+    end
   end
 end
