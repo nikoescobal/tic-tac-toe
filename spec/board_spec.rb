a6e14d57244c614714684d25c63030af155fd1d8
Niko Escobal <niko.escobal@gmail.com>
2021-02-11 23:30:58 +0800

    Added spec tests for player.rb file.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,46 +1,44 @@
 require_relative '../lib/board'
 require_relative '../lib/player'
 
-describe Board do  
+describe Board do
+  draw_matrix = [
+    %w[x o x],
+    %w[o o x],
+    %w[x x o]
+  ]
 
-draw_matrix = [
-  ["x", "o", "x"],  
-  ["o", "o", "x"],
-  ["x", "x", "o"]
-]
+  win_matrix1 = [
+    %w[x x x],
+    %w[o o x],
+    %w[o x o]
+  ]
 
-win_matrix_1 = [
-  ["x", "x", "x"],  
-  ["o", "o", "x"],
-  ["o", "x", "o"]
-]
+  win_matrix2 = [
+    %w[o o x],
+    %w[o o x],
+    %w[x x o]
+  ]
 
-win_matrix_2 = [
-  ["o", "o", "x"],  
-  ["o", "o", "x"],
-  ["x", "x", "o"]
-]
+  player1 = Player.new('Chad', 'x')
+  player2 = Player.new('Jake', 'o')
 
-  player1 = Player.new("Chad", "x")
-  player2 = Player.new("Jake", "o")
-
-  describe "#add_position" do
-    it "adds the selected position to the board" do
+  describe '#add_position' do
+    it 'adds the selected position to the board' do
       board = Board.new(player1, player2)
-      board.add_position(1, 1, "o")
-      expect(board.matrix[1][1]).to eq("o")
+      board.add_position(1, 1, 'o')
+      expect(board.matrix[1][1]).to eq('o')
     end
 
-    it "fails to add the selected position to the board" do
+    it 'fails to add the selected position to the board' do
       board = Board.new(player1, player2)
-      board.add_position(1, 1, "o")
-      expect(board.matrix[1][1]).to_not eq("x")
+      board.add_position(1, 1, 'o')
+      expect(board.matrix[1][1]).to_not eq('x')
     end
-
   end
 
-  describe "#turn_counter" do
-    it "counts the number of turns" do
+  describe '#turn_counter' do
+    it 'counts the number of turns' do
       board = Board.new(player1, player2)
       board.turn_counter
       expect(board.counter).to eq(1)
@@ -72,51 +70,48 @@ win_matrix_2 = [
     end
   end
 
-  describe "#draw" do
-
-    it "checks if the game is a draw" do
+  describe '#draw' do
+    it 'checks if the game is a draw' do
       board = Board.new(player1, player2)
       board.matrix = draw_matrix
       flag = board.draw
       expect(flag).to eq(true)
     end
 
-    it "checks if the game is not a draw" do
+    it 'checks if the game is not a draw' do
       board = Board.new(player1, player2)
       flag = board.draw
       expect(flag).to eq(false)
     end
   end
 
-  describe "#win" do
-
-    it "checks if player 1 has won the game" do
+  describe '#win' do
+    it 'checks if player 1 has won the game' do
       board = Board.new(player1, player2)
-      board.matrix = win_matrix_1
-      flag = board.win("x")
+      board.matrix = win_matrix1
+      flag = board.win('x')
       expect(flag).to eq(true)
     end
 
-    it "checks if player 2 has won the game" do
+    it 'checks if player 2 has won the game' do
       board = Board.new(player1, player2)
-      board.matrix = win_matrix_2
-      flag = board.win("o")
+      board.matrix = win_matrix2
+      flag = board.win('o')
       expect(flag).to eq(true)
     end
 
-    it "checks if the game is not a win" do
+    it 'checks if the game is not a win' do
       board = Board.new(player1, player2)
       board.matrix = draw_matrix
-      flag = board.win("x")
+      flag = board.win('x')
       expect(flag).to eq(false)
     end
   end
 
-  describe "#input_valid?" do
-
-    it "checks if the input for a move is valid" do
+  describe '#input_valid?' do
+    it 'checks if the input for a move is valid' do
       board = Board.new(player1, player2)
-      flag = board.input_valid?(1,1)
+      flag = board.input_valid?(1, 1)
       expect(flag).to eq(true)
     end
   end
